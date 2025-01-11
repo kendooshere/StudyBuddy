abecf7fbeebe8abf8a5d6db9189d92e0adcd3abb
angad <121285790+kendooshere@users.noreply.github.com>
2025-01-12 01:21:41 +0530

    Corrected a songs name in js

--- js/script.js
+++ js/script.js
@@ -156,7 +156,7 @@ let trackList = [
 		name: "Chauffeur",
 		artist: "Diljit Dosanjh",
 		image: "https://th.bing.com/th/id/OIP.wDSiynYZJ8WTiWtjlBVsmgHaDt?w=282&h=174&c=7&r=0&o=5&pid=1.7",
-		path: "sounds/Chauffeur.mp3"
+		path: "sounds/chauffeur.mp3"
 	},
 	{
 		name: "New person same old mistakes",
