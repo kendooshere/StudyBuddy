5140402a4199236df4e65cc0c1239f33aee2988d
Angad <121285790+kendooshere@users.noreply.github.com>
2025-03-29 05:59:52 +0000

    Added code for Firebase auth

---
+++ js/register.js
@@ -0,0 +1,16 @@
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const auth = getAuth();
+
+document.getElementById("registerForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("Account Created! Redirecting...");
+            window.location.href = "dashboard.html";
+        })
+        .catch((error) => alert(error.message));
+});
