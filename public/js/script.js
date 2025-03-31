fa2893130dcf16653a0fe5324d5d79137c215849
Angad <121285790+kendooshere@users.noreply.github.com>
2025-03-31 05:48:51 +0000

    Removed unwanted code from script.js

--- public/js/script.js
+++ public/js/script.js
@@ -309,20 +309,3 @@ function playerToggle() {
 	}
 }
 
-//firebase integration//
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyDBXXlkRJkgrvABphpV75x02sXpHlJ2oko",
-    authDomain: "studybuddy-51261.firebaseapp.com",
-    projectId: "studybuddy-5126",
-    storageBucket: "studybuddy-51261.appspot.com",
-    messagingSenderId: "965042095610",
-    appId: "1:965042095610:web:635dd4f0e0a48f7de55043"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
-  const db = getFirestore();
