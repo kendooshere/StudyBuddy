1fa5faa9315874831af48cebf839d59824d4d284
Angad <121285790+kendooshere@users.noreply.github.com>
2025-03-31 06:01:49 +0000

    Added auth.js with firebase authentication

---
+++ public/js/auth.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDBXXlkRJkgrvABphpV75x02sXpHlJ2oko",
+    authDomain: "studybuddy-51261.firebaseapp.com",
+    projectId: "studybuddy-5126",
+    storageBucket: "studybuddy-51261.appspot.com",
+    messagingSenderId: "965042095610",
+    appId: "1:965042095610:web:635dd4f0e0a48f7de55043"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// FirebaseUI Config
+const uiConfig = {
+  signInSuccessUrl: "home.html", // Redirect after login
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+};
+
+// Initialize FirebaseUI
+const ui = new firebaseui.auth.AuthUI(auth);
+ui.start("#firebase-auth-container", uiConfig);
